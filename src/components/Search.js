118e8045a41599b3b355e2b38e5594d13fd6a249
Ben Quinn <benjamesquinn@gmail.com>
2022-06-12 19:34:14 +0100

    Added GET request Search form functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,20 +1,29 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import "../styles/search.css";
+import getImages from "../requests/getImages";
 
 function Search() {
   const [value, setValue] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getImages(value);
+  };
+
   return (
-    <form>
-      <input
-        type="text"
-        className="search__input"
-        onChange={(event) => setValue(event.target.value)}
-      />
-      <button className="search__button" type="submit">
-        Search
-      </button>
-    </form>
+    <div className="search">
+      <form className="search__form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="search__input"
+          onChange={(event) => setValue(event.target.value)}
+        />
+        <button className="search__button" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
   );
 }
 
