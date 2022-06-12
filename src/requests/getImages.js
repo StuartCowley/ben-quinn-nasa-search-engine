7686d652ad1305ca227a6d9cc6adf34e7a1b1ca3
Ben Quinn <benjamesquinn@gmail.com>
2022-06-12 20:10:07 +0100

    Improved search functionailty to return array of image hrefs

--- src/requests/getImages.js
+++ src/requests/getImages.js
@@ -7,7 +7,12 @@ const getImages = (query) => {
   return axios
     .get(`https://images-api.nasa.gov/search?q=${query}`)
     .then((res) => {
-      console.log(res);
+      const imageResults = res.data.collection.items;
+      const images = imageResults
+        .filter((item) => item.data[0].media_type === "image")
+        .map((image) => image.links[0].href);
+      console.log(images);
+      return images;
     })
     .catch((err) => {
       console.log(err);
