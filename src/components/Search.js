16a56a77767a0e999681a15f1986e1b7047c993f
Ben Quinn <benjamesquinn@gmail.com>
2022-06-12 15:19:53 +0100

    Added form to Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,19 @@ import React, { useState } from "react";
 import "../styles/search.css";
 
 function Search() {
-  return <input type="text" className="search__input" />;
+  const [value, setValue] = useState();
+  return (
+    <form>
+      <input
+        type="text"
+        className="search__input"
+        onChange={(event) => setValue(event.target.value)}
+      />
+      <button className="search__button" type="submit">
+        Search
+      </button>
+    </form>
+  );
 }
 
 export default Search;
