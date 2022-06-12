04694b23b4684ef1107326b6cd0c44e39d518fea
Ben Quinn <benjamesquinn@gmail.com>
2022-06-12 21:54:56 +0100

    Improved SearchResults functionality to return images

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 import React from "react";
 import "../styles/SearchResults.css";
 import PropTypes from "prop-types";
@@ -9,12 +10,15 @@ function SearchResults({ results }) {
   return (
     <>
       <p>Search results:</p>
-      <img
-        data-testid="search-results__image"
-        className="search-results__image"
-        alt="card"
-        src="https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-      />
+      {results.map((item) => (
+        <img
+          key={results.indexOf(item)}
+          data-testid={`search-results__image${results.indexOf(item)}`}
+          className={`search-results__image${results.indexOf(item)}`}
+          alt="space"
+          src={item}
+        />
+      ))}
     </>
   );
 }
